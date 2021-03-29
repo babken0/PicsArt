17f9b9405c3b1f00591c0269d57282af3f80d75b
babken0 <babkenboyaxchyan@gmail.com>
2021-03-29 23:33:37 +0400

    Initial commit

---
+++ src/primitiveTypes/Types.java
@@ -0,0 +1,14 @@
+package primitiveTypes;
+
+public class Types {
+    public static void main(String[] args) {
+        byte b = -128;
+        short s = 200;
+        int i = 1234568;
+        long l = 122222555544L;
+        float f = 1.75784f;//7
+        double d = 1.122555554;//15-16
+        System.out.println(d);
+
+    }
+}
